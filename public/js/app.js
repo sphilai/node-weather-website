47887aec3eb6f8bffeb08f782accb77abb4ab1a5
Sasipa Philai <sasipa.philai@gmail.com>
2020-02-01 20:44:10 +0700

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 
 })
 
-fetch('http://localhost:3000/weather?address=london').then((response)=>{
+fetch('/weather?address=london').then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         {
